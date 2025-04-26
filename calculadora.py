e31ef345b76dbc69f0d06ebd92902e637b4f5c2a
Francisco J <franbecpin@gmail.com>
2025-04-26 19:07:45 +0200

    UMA: Tema1-Ejercicio1

---
+++ calculadora.py
@@ -0,0 +1,31 @@
+# Autor: Francisco J. Becerra
+# Fecha: 26/04/2025
+# Curso: UMA - Python para principiantes
+# Tema: 1
+# Ejercicio: 1
+
+# Descripción:
+'''
+Escribir un programa que pregunte al usuario
+un número de horas y el precio por hora para
+calcular el total a pagar.
+Introduzca las horas: 35
+Introduzca el precio/hora: 2.75
+Total: 96.25
+'''
+
+# Variables
+numHoras = 0
+precioHora = 0
+
+# Introducir datos con conversion
+print('------------------------')
+print('Calculo de Total a Pagar')
+print('------------------------')
+numHoras = float(input('Introduzca las horas: '))
+precioHora = float(input('Introduzca el precio/hora: '))
+
+
+# Salida con conversion
+print('-----------')
+print('Total: '+str(numHoras*precioHora))
